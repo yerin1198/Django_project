8e0c7db7c099dc247084dbd1c15253d567ae9898
yerin1198 <yerin1198@naver.com>
2022-09-06 14:31:40 +0900

    상품 조회/수정/삭제 구현

--- product/urls.py
+++ product/urls.py
@@ -6,5 +6,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('v1/product/', views.ProductList.as_view()),
+    path('v1/<int:pk>/', views.ProductDetail.as_view()),
     path('v1/test/', TestView.as_view(), name='test'),
 ]
--- product/views.py
+++ product/views.py
@@ -26,3 +26,20 @@ class ProductList(mixins.CreateModelMixin,
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+
+class ProductDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
