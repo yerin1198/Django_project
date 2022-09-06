da6695da0131ac75e4c941ae4c53acf82eb80f11
yerin1198 <yerin1198@naver.com>
2022-09-06 19:26:32 +0900

    오류 고침

--- product/filters.py
+++ product/filters.py
@@ -4,20 +4,29 @@ from product.models import Product
 
 
 class ProductFilter(FilterSet):
-    class Meta:
-        price = NumberFilter()
-        price__gt = NumberFilter(field_name='price', lookup_expr="gt")
-        price__lt = NumberFilter(field_name='price', lookup_expr="lt")
-        description = CharFilter(field_name='description', lookup_expr="icontains")  # 문자열 포함
 
-        class Meta:
-            model = Product
-            fields = ["price", "description"]
+    price = NumberFilter()
+    price__gt = NumberFilter(field_name="price", lookup_expr="gt")
+    price__lt = NumberFilter(field_name="price", lookup_expr="lt")
+    description = CharFilter(field_name="description", lookup_expr="icontains")  # 문자열 포함
 
-        order_by_field = 'ordering'
-        ordering = OrderingFilter(
-            fields=(
-                'price',
-            )
+    order_by_field = 'ordering'
+    ordering = OrderingFilter(
+        fields=(
+            'price',
         )
+    )
+
+    class Meta:
+        model = Product
+        fields = {
+            'description': ['exact', 'contains']
+        }
+
+
+
+
+
+
+
 
