7c4c43ffacfba53e0fade2a49faa882871d1ba1d
yerin Kim <yerin1198@fount.co>
2022-09-07 16:38:11 +0900

    필터링에 이름,상세설명 조건 추가

--- product/views.py
+++ product/views.py
@@ -21,8 +21,15 @@ class ProductViewSet(viewsets.ModelViewSet):
     pagination_class = ProductPagination
 
     def get_queryset(self):  # get_queryset 재정의
-        q = self.request.query_params.get('search', '')
+        q = self.request.query_params.get('price', '')
+        name_q = self.request.query_params.get('name', '')
+        description_q = self.request.query_params.get('description', '')
         qs = super().get_queryset()
         if q:
             qs = qs.filter(price__gt=q)
+        if name_q:
+            qs = qs.filter(name__icontains=name_q)
+        if description_q:
+            qs = qs.filter(description__icontains=description_q)
+
         return qs
