8aafff3ca84ccacf2354a3e452911537e5704501
yerin Kim <yerin1198@fount.co>
2022-09-07 15:32:49 +0900

    페이징 클래스 리팩토링

--- product/views.py
+++ product/views.py
@@ -4,15 +4,12 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import filters
 
-from product.filters import ProductFilter
 from product.models import Product
+from product.pagination import ProductPagination
 from product.serializers import ProductSerializer
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import PageNumberPagination
 
 
-class ProductPagination(PageNumberPagination):
-    page_size = 4
+
 
 
 class TestView(APIView):
@@ -25,15 +22,11 @@ class TestView(APIView):
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-    filter_backends = [filters.OrderingFilter]
     pagination_class = ProductPagination
-    ordering_fields = ['price']  # 정렬 대상이 될 field 지정
-    # ordering = ['id']  # Default 정렬 기준 지정
 
     def get_queryset(self):  # get_queryset 재정의
         q = self.request.query_params.get('search', '')
         qs = super().get_queryset()
         if q:
-            qs = qs.filter(price__gt=q)
-            print(qs)
+            qs = qs.filter(price__gt=q).order_by('price')
         return qs
