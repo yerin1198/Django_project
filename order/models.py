d1f7fd8ecfccfc80b59c5d89f2e6d8424dfb1226
yerin Kim <yerin1198@fount.co>
2022-09-14 12:09:22 +0900

    주문 로직 구현

--- order/models.py
+++ order/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 
 class Order(models.Model):
-    user = models.ForeignKey('user.User', verbose_name="사용자", on_delete=models.CASCADE)
-    product = models.ForeignKey('product.Product', verbose_name="상품", on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', verbose_name="사용자", on_delete=models.CASCADE) #참조하는 대상의 id값을 가져옴.
+    product = models.ForeignKey('product.Product', verbose_name="상품", on_delete=models.CASCADE) #장고에서 1:n 관계에서 fk는 n쪽에 명시.
     registered_date = models.DateTimeField(auto_now_add=True, verbose_name="등록시간")
     quantity = models.IntegerField(verbose_name="수량")
 
     def __str__(self):  # 객체를 생성하면 문자열로 포함된 함수인 __str__이 호출됨.
         return str(self.user) + ' ' + str(self.product)
 
-    class Meta:
-        db_table = 'my_order'
+
 
--- order/views.py
+++ order/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from order.models import Order
+from order.serializers import OrderSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]  # 권한 추가
